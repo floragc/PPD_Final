b89de41b87a103d8151c537dd47cb49e99decbf8
Flora Guy-Coichard <flora.guycoichard@gmail.com>
2015-06-30 21:21:18 +0200

    Revert "Merging versions"
    This reverts commit 0c660d2aa930d8d86f6405cb63b6bad51768343e.

--- APP/routes/maladies.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/maladies', function(req, res, next) {
-  res.render('maladies', { requete_param: req.id });
-});
-
-module.exports = router;
