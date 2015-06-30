52095987006935f2d21dab53c8513954b3e2c1de
Flora Guy-Coichard <flora.guycoichard@gmail.com>
2015-06-30 21:24:19 +0200

    Revert dbeb90e..57eccb2
    This rolls back to commit dbeb90e50fb85493c771adea4d75befd469b291a.

--- APP/routes/genes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/genes', function(req, res, next) {
-  res.render('genes', { requete_param: req.id });
-});
-
-module.exports = router;
--- APP/routes/maladie.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/maladie/:id', function(req, res, next) {
-  res.render('maladie', { requete_param: req.id });
-});
-
-module.exports = router;
